319cce82e7568d098233b3acc6dfa1ade8878edb
4rt3n51 <artenis.islami@fshnstudent.info>
2026-06-04 14:12:43 +0200

    small improvement

--- module/main.tf
+++ module/main.tf
@@ -501,8 +501,3 @@ resource "aws_cloudtrail" "this" {
     }
   }
 }
-
-resource "aws_s3_bucket_policy" "this" {
-  bucket = aws_s3_bucket.this.id
-  policy = data.aws_iam_policy_document.bucket_policy.json
-}
