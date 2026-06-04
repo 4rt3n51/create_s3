a0f9783f9d4b60e7e61ec1b7b9dae695edab39fe
4rt3n51 <artenis.islami@fshnstudent.info>
2026-06-04 14:14:13 +0200

    small change for the time

--- module/main.tf
+++ module/main.tf
@@ -1,6 +1,6 @@
 locals {
   normalized_bucket_base = substr(trim(replace(lower(var.bucket_name), "/[^a-z0-9-]/", "-"), "-"), 0, 47)
-  final_bucket_name      = "${local.normalized_bucket_base}-${formatdate("YYYYMMDD-HHMMSS", time_static.created.rfc3339)}"
+  final_bucket_name      = "${local.normalized_bucket_base}-${formatdate("YYYYMMDD-HHMM", time_static.created.rfc3339)}"
 }
 
 resource "time_static" "created" {}
