e1f82ab82cc46c0bbf41af1ce972e50690ea039f
4rt3n51 <artenis.islami@fshnstudent.info>
2026-06-04 14:12:13 +0200

    change small

--- module/main.tf
+++ module/main.tf
@@ -506,12 +506,3 @@ resource "aws_s3_bucket_policy" "this" {
   bucket = aws_s3_bucket.this.id
   policy = data.aws_iam_policy_document.bucket_policy.json
 }
-
-resource "aws_s3_bucket_public_access_block" "this" {
-  bucket = aws_s3_bucket.this.id
-
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
